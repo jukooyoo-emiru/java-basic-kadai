1c412b907c9a53e6856678cb7c050d8f5f6693eb
jukooyoo-emiru <emirchmd@gmail.com>
2026-07-29 04:33:33 +0900

    kadai_015をアップロード

---
+++ kadai_015/Car_Chapter15.java
@@ -0,0 +1,24 @@
+package kadai_015;
+
+public class Car_Chapter15 {
+	private int gear  = 1;
+	private int speed = 10;
+	
+	public void changeGear( int afterGear ) {
+		this.speed = switch (afterGear) {
+		case 1 -> 10;
+		case 2 -> 20;
+		case 3 -> 30;
+		case 4 -> 40;
+		case 5 -> 50;
+		default -> 10;
+		};
+		if( (1 <= afterGear) && (afterGear <=5) ) {
+			this.gear = afterGear;
+		}
+	}
+	
+	public void run() {
+		System.out.println("現在の速度：時速" + this.speed + "kmです");
+	}
+}
