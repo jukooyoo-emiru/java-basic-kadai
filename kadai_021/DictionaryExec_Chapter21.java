b16cca6a18d8116618f457b5ce87ac2ffdcb04bb
jukooyoo-emiru <emirchmd@gmail.com>
2026-08-07 01:46:27 +0900

    kadai_021をアップロード

---
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,15 @@
+package kadai_021;
+
+public class DictionaryExec_Chapter21 {
+
+	public static void main(String[] args) {
+		Dictionary_Chapter21 dictWord = new Dictionary_Chapter21();
+		
+		dictWord.createDictionary();
+		
+		String[] engWord = {"apple", "banana", "grape", "orange"};
+		
+		dictWord.searchWord(engWord);
+	}
+
+}
